23a9ff74b8f4a3b9f0336644f5e25dddea26a6fc
Upadhyay-codes <abhishek24448@gmail.com>
2026-06-16 16:08:37 +0530

    news website for newsreader

---
+++ learning.js
@@ -0,0 +1,14 @@
+// let els = document.getElementById("video-card");
+// console.log(els)
+//  let name = prompt("SORRY THIS WEB (NEWS MANIA) SOLD TO SUMIT DEFALTER IN ONE PREMIUM MOMO PLATE...");
+// let p= fetch("https://api.rss2json.com/v1/api.json?rss_url=https://feeds.bbci.co.uk/news/rss.xml")
+// p.then((value1) => {
+//     console.log(value1.status)
+//     console.log(value1.ok)
+//     return value1.json() 
+// }).then((value2) => {
+//         console.log(value2)
+//     })
+let charArray = ["d","h","r","u","v"];
+charArray.reverse();
+console.log(charArray);
